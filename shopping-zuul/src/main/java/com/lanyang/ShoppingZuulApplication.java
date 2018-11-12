0dd73fd4c2af2a50db526c7bfc4ae4c2526da29d
HiLany <lanyang@pivotal.net.cn>
2018-11-12 22:00:27 +0800

    add zuul proxy for shopping

---
+++ shopping-zuul/src/main/java/com/lanyang/ShoppingZuulApplication.java
@@ -0,0 +1,18 @@
+package com.lanyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ShoppingZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingZuulApplication.class, args);
+	}
+}
